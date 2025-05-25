5313a8e7b453d752bbad7b2cd17149963537ced0
karthikpk20046 <164026979+karthikpk20046@users.noreply.github.com>
2025-05-25 13:27:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import Layout from './components/Layout';
